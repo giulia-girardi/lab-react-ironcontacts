66ac76a75cb902d1ec44a85358c9201e35766959
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-15 17:14:16 +0100

    done to iter 5 but with bugs

--- src/App.js
+++ src/App.js
@@ -11,12 +11,41 @@ const remainingContacts = contactsJSON.slice(6,contactsJSON.length)
 
 
 function App() {
-  const randomIndex = Math.floor(Math.random() * remainingContacts.length)
-  const randomContact = contactsJSON[randomIndex]
-  const [contact, setContact] = useState('0')
+  
+  
+  const [contacts, setContact] = useState(fiveContacts)
+
+  const addRandom = () => {
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length)
+    const randomContact = remainingContacts[randomIndex]
+    const contactsWithoutDuplicates = remainingContacts.splice(randomIndex + 1)
+    console.log(contactsWithoutDuplicates)
+    setContact([...contacts, randomContact])
+  }
+
+  const sortPopularity = () => {
+    let copyContacts = [...contacts]
+    let sortedContacts = copyContacts.sort((a, b) => b.popularity - a.popularity)
+    setContact(sortedContacts)
+  }
+
+  const sortAlphabetically = () => {
+    let copyContacts = [...contacts]
+    let sortedContacts = copyContacts.sort((a, b) => a.name.toLowerCase().localeCompare(b.name))
+    setContact(sortedContacts)
+  }
+
+  const deleteItem = (id) => {
+    let copyContacts = [...contacts]
+    let listWithoutDeleted = copyContacts.filter((item) => item.id !== id)
+    setContact(listWithoutDeleted)
+  }
+
   return (
     <div className="App">
-      <button onClick={()=> setContact(fiveContacts.push(randomContact))}>Add random contact</button>
+      <button onClick={addRandom}>Add random contact</button>
+      <button onClick={sortPopularity} >Sort by popularity</button>
+      <button onClick={sortAlphabetically}>Sort by name</button>
       <table>
         <thead>
           <tr>
@@ -25,17 +54,19 @@ function App() {
             <th>Popularity</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {fiveContacts.map((contact)=> {
+          {contacts && contacts.map((contact)=> {
             return (
-              <tr key={uuidv4()}>
+              <tr key={contact.id}>
                 <td><img src={contact.pictureUrl}/></td>
                 <td><p>{contact.name}</p></td>
                 <td><p>{(contact.popularity).toFixed(2)}</p></td>
                 <td><p>{contact.wonOscar ? "🏆" : ""}</p></td>
                 <td><p>{contact.wonEmmy ? "🏆" : ""}</p></td>
+                <td><button onClick={()=> deleteItem(contact.id)}>Delete</button></td>
               </tr>
             )
           })}
